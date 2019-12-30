6f68ca5c1cce3bfafd17412676bab6e58db081a5
Yuhan Liu <yliu1021@outlook.com>
2019-12-30 04:00:39 -0500

    Profiles run loop

--- live_test.py
+++ live_test.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from tensorflow.keras.models import load_model as keras_load_model
 import cv2
@@ -26,11 +27,12 @@ def main():
     plt.xticks(rotation=90)
     plt.tight_layout()
 
-    num_frames = 10
-
+    num_frames = 8
+    frame_time = 1/MIN_FPS * 1000
+    
     single_frame_model = model.single_frame_model()
     multi_frame_model = model.multi_frame_model(single_frame_model, num_frames=num_frames, stateful=True)
-    multi_frame_model.load_weights(os.path.join('training', 'run4', 'multi_frame_model.35.hdf5'))
+    multi_frame_model.load_weights(os.path.join('training', 'run5', 'multi_frame_model.12.hdf5'))
 
     cap = cv2.VideoCapture(0)
 
@@ -38,27 +40,50 @@ def main():
     model_input = np.zeros((1, num_frames, IMAGE_HEIGHT, IMAGE_WIDTH, 3), dtype=np.float32)
 
     def animate(i):
+        start = time.time()
         ret, frame = cap.read()
+        read_time = time.time() - start
+        
         if not ret:
             print("Couldn't read input")
             return
-
+        
+        start = time.time()
         frame = cv2.resize(frame, image_size)
+        resize_time = time.time() - start
+        
+        start = time.time()
         model_input[0, :-1] = model_input[0, 1:]
         model_input[0, -1] = frame
+        input_shift_time = time.time() - start
+        
+        start = time.time()
         pred = multi_frame_model.predict(model_input)
+        predict_time = time.time() - start
+        
+        start = time.time()
         pred = softmax(pred[0, -1])
+        softmax_time = time.time() - start
 
         for bar, p in zip(bars, pred):
             bar.set_height(p)
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
-        if cv2.waitKey(int(1/MIN_FPS * 1000)) & 0xFF == ord('q'):
+        if cv2.waitKey(max(1, int((time.time() - start)*1000 - frame_time))) & 0xFF == ord('q'):
             cap.release()
             cv2.destroyAllWindows()
             exit(0)
 
+        print('Read: {:.3f}\nResize: {:.3f}\nShift: {:.3f}\nPredict: {:.3f}\nSoftmax: {:.3f}\nTotal: {:.3f}\n\n'.format(
+                read_time,
+                resize_time,
+                input_shift_time,
+                predict_time,
+                softmax_time,
+                read_time + resize_time + input_shift_time + predict_time + softmax_time
+            )
+        )
         return bars
 
     animation.FuncAnimation(fig, animate, frames=None, interval=1, blit=True)
