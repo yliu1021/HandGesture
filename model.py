66dd14043eedd9a543e11ac725d02008015dc7b1
Yuhan Liu <yliu1021@outlook.com>
2020-01-14 06:41:22 -0800

    flatten single frame encoding

--- model.py
+++ model.py
@@ -113,22 +113,19 @@ def single_frame_model():
 
     x = reductionB(x)
 
+    x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+
     return Model(frame_input, x, name='single_frame_encoder')
 
 
 def multi_frame_model(num_frames=None):
-    encoded_frame_input = Input(shape=(num_frames, 4, 6, 2048))
+    encoded_frame_input = Input(shape=(num_frames, 1024))
 
-    x = TimeDistributed(Dense(512, activation='relu'))(encoded_frame_input)
-    x_diff = Lambda(lambda x: x[:, 1:] - x[:, :-1])(x)
-    x_offset = Lambda(lambda x: x[:, 1:])(x)
-    x = Concatenate(axis=-1)([x_diff, x_offset])
-    x = TimeDistributed(Flatten())(x)
-    x = TimeDistributed(Dense(1024, activation='relu'))(x)
-
-    filter_sizes = [512, 256, 128]
+    x = SeparableConv1D(1024, kernel_size=3, activation='relu', padding='valid')(encoded_frame_input)
+    filter_sizes = [1024, 1024, 1024]
     for filter_size in filter_sizes:
-        x = Conv1D(filter_size, kernel_size=3, activation='relu', padding='valid')(x)
+        x = SeparableConv1D(filter_size, kernel_size=3, activation='relu', padding='valid')(x)
 
     x = Dense(NUM_CLASSES)(x)
     return Model(encoded_frame_input, x, name='multi_frame_model')
@@ -146,14 +143,14 @@ def full_model(num_frames=None):
 
 
 if __name__ == '__main__':
-    single_frame_encoder, multi_frame_encoder, model = full_model(num_frames=8)
+    single_frame_encoder, multi_frame_encoder, model = full_model(num_frames=9)
     single_frame_encoder.summary()
     multi_frame_encoder.summary()
 
     FRAMES = 10
     start = time.time()
     for i in range(FRAMES):
-        print(model.predict(np.zeros(shape=(1, 8, 108, 192, 3))).shape)
+        print(model.predict(np.zeros(shape=(1, 9, 108, 192, 3))).shape)
     end = time.time()
     print((end - start)/FRAMES)
     
