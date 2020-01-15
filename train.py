e7fd6d6aa41ba4428749e3e8edd16af1a5f15e91
Yuhan Liu <yliu1021@outlook.com>
2020-01-14 19:13:53 -0800

    remove print

--- train.py
+++ train.py
@@ -137,7 +137,6 @@ def main(should_prune=False):
             sparsity.PruningSummaries(log_dir=pruning_dir)
         ])
 
-    print(callbacks)
     hist = full_model.fit(
         train_data_generator,
         steps_per_epoch=steps_per_epoch,
