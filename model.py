91eb99156f12785047491505ad869e928eecb9a5
Yuhan Liu <yliu1021@outlook.com>
2020-01-14 17:05:38 -0800

    added more batch renorm

--- model.py
+++ model.py
@@ -21,6 +21,7 @@ def stem(x):
     b1 = MaxPooling2D(pool_size=(3, 3), strides=2, padding='same')(x)
     b2 = Conv2D(filters=32, kernel_size=3, strides=2, activation='relu', padding='same')(x)
     x = Concatenate(axis=-1)([b1, b2])
+    x = BatchNormalization(renorm=True)(x)
 
     b1 = Conv2D(filters=64, kernel_size=1, activation='relu', padding='same')(x)
     b1 = Conv2D(filters=96, kernel_size=3, activation='relu', padding='same')(b1)
@@ -29,6 +30,7 @@ def stem(x):
     b2 = Conv2D(filters=64, kernel_size=(1, 7), activation='relu', padding='same')(b2)
     b2 = Conv2D(filters=96, kernel_size=3, activation='relu', padding='same')(b2)
     x = Concatenate(axis=-1)([b1, b2])
+    x = BatchNormalization(renorm=True)(x)
 
     b1 = Conv2D(filters=192, kernel_size=3, strides=2, activation='relu', padding='same')(x)
     b2 = MaxPooling2D(pool_size=(2, 2), strides=2, padding='same')(x)
@@ -79,6 +81,7 @@ def reductionA(x):
     b3 = Conv2D(filters=384, kernel_size=3, strides=2, activation='relu', padding='same')(b3)
 
     x = Concatenate(axis=-1)([b1, b2, b3])
+    x = BatchNormalization(renorm=True)(x)
     return x
 
 
@@ -96,6 +99,7 @@ def reductionB(x):
     b4 = Conv2D(filters=256, kernel_size=3, strides=2, activation='relu', padding='same')(b4)
 
     x = Concatenate(axis=-1)([b1, b2, b3, b4])
+    x = BatchNormalization(renorm=True)(x)
     return x
 
 
