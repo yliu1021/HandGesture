ca612c6609d925e0f79e02b74030519d38e18ebb
Yuhan Liu <yliu1021@outlook.com>
2020-02-10 04:27:33 -0800

    reduced augmentation

--- data.py
+++ data.py
@@ -116,9 +116,9 @@ class DataSet:
         images = np.array([cv2.resize(cv2.imread(x), img_size) for x in image_files])
         images = images / 255.0
 
-        f = 2 ** random.gauss(0, 0.25)
-        images = images * f + random.gauss(0, 0.25)
-        images += np.random.normal(0, 0.1, size=images.shape)
+        f = 2 ** random.gauss(0, 0.15)
+        images = images * f + random.gauss(0, 0.15)
+        images += np.random.normal(0, 0.02, size=images.shape)
         return images
 
     @staticmethod
